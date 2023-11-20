e0f8187426e51c1ac3564f191298074c63c4294a
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-11-20 19:38:21 +0530

    something

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./Hero.css";
 import { useState } from "react";
 import memesData from "./memesData";
-
+import seedrandom from "seedrandom";
 function Hero() {
   const [memeImage, setMemeImage] = React.useState("");
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const rng = seedrandom();
+    const randomNumber = Math.floor(rng() * memesArray.length);
     setMemeImage(memesArray[randomNumber].url);
   }
+
   return (
     <div className="hero">
       <div className="hero-input">
